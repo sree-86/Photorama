38a1964bb8dbf056c8322e3ad7e81f52754b68f3
sree-86 <sriram3rhyme@gmail.com>
2019-03-14 19:14:41 -0400

    Image View

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Sreeram Ramakrishnan on 2019-03-14.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController{
+    @IBOutlet var imageView: UIImageView!
+}
